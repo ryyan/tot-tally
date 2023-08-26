0b327e0555387cdb84284841486cda3f69e0f936
ryyan <ryyan@pm.me>
2023-08-25 22:40:40 -0500

    Add time since last

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
@@ -45,10 +46,12 @@ var (
 )
 
 type TallyPageData struct {
-	Name     string
-	Timezone string
-	Feeds    []Feed
-	Soils    []Soil
+	Name              string
+	Timezone          string
+	Feeds             []Feed
+	Soils             []Soil
+	TimeSinceLastFeed string
+	TimeSinceLastSoil string
 }
 
 type Feed struct {
@@ -208,13 +211,22 @@ func getTallyPageData(baby_id string) (TallyPageData, error) {
 		formattedFeeds[i] = Feed{formattedTime, feed.Note, ounces_str}
 	}
 
+	timeSinceLastFeed := "N/A"
+	if len(listFeeds) > 0 {
+		timeSinceLastFeed = timeElapsed(time.Now().UTC(), listFeeds[0].CreatedAt, false)
+	}
+
 	// Get and format list of Soils
 	log.Printf("ListSoils baby_id=%s\n", baby_id)
 	listSoils, err := queries.ListSoils(ctx, baby_id)
 	if err != nil {
 		return TallyPageData{}, err
 	}
-	log.Printf("%s\n", listSoils)
+
+	timeSinceLastSoil := "N/A"
+	if len(listSoils) > 0 {
+		timeSinceLastSoil = timeElapsed(time.Now().UTC(), listSoils[0].CreatedAt, false)
+	}
 
 	formattedSoils := make([]Soil, len(listSoils))
 	for i, soil := range listSoils {
@@ -222,7 +234,7 @@ func getTallyPageData(baby_id string) (TallyPageData, error) {
 		formattedSoils[i] = Soil{formattedTime, soil.Note, soil.Wet, soil.Soil}
 	}
 
-	data := TallyPageData{Name: getBaby.Name, Timezone: getBaby.Timezone, Feeds: formattedFeeds, Soils: formattedSoils}
+	data := TallyPageData{Name: getBaby.Name, Timezone: getBaby.Timezone, Feeds: formattedFeeds, Soils: formattedSoils, TimeSinceLastFeed: timeSinceLastFeed, TimeSinceLastSoil: timeSinceLastSoil}
 	return data, nil
 }
 
@@ -285,4 +297,75 @@ func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func s(x float64) string {
+	if int(x) == 1 {
+		return ""
+	}
+	return "s"
+}
+
+// https://socketloop.com/tutorials/golang-human-readable-time-elapsed-format-such-as-5-days-ago
+func timeElapsed(now time.Time, then time.Time, full bool) string {
+	var parts []string
+	var text string
+
+	year2, month2, day2 := now.Date()
+	hour2, minute2, second2 := now.Clock()
+
+	year1, month1, day1 := then.Date()
+	hour1, minute1, second1 := then.Clock()
+
+	year := math.Abs(float64(int(year2 - year1)))
+	month := math.Abs(float64(int(month2 - month1)))
+	day := math.Abs(float64(int(day2 - day1)))
+	hour := math.Abs(float64(int(hour2 - hour1)))
+	minute := math.Abs(float64(int(minute2 - minute1)))
+	second := math.Abs(float64(int(second2 - second1)))
+
+	week := math.Floor(day / 7)
+
+	if year > 0 {
+		parts = append(parts, strconv.Itoa(int(year))+" year"+s(year))
+	}
+
+	if month > 0 {
+		parts = append(parts, strconv.Itoa(int(month))+" month"+s(month))
+	}
+
+	if week > 0 {
+		parts = append(parts, strconv.Itoa(int(week))+" week"+s(week))
+	}
+
+	if day > 0 {
+		parts = append(parts, strconv.Itoa(int(day))+" day"+s(day))
+	}
+
+	if hour > 0 {
+		parts = append(parts, strconv.Itoa(int(hour))+" hour"+s(hour))
+	}
+
+	if minute > 0 {
+		parts = append(parts, strconv.Itoa(int(minute))+" minute"+s(minute))
+	}
+
+	if second > 0 {
+		parts = append(parts, strconv.Itoa(int(second))+" second"+s(second))
+	}
+
+	if now.After(then) {
+		text = " ago"
+	} else {
+		text = " after"
+	}
+
+	if len(parts) == 0 {
+		return "just now"
+	}
+
+	if full {
+		return strings.Join(parts, ", ") + text
+	}
+	return parts[0] + text
 }
